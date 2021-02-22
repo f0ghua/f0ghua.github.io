3bbd5299c4991652669e8616dc87cefcb2d2cb9d
f0g <fog.hua@gmail.com>
2021-02-22 20:20:56 +0800

    Nikola auto commit.
    Nikola version: 8.1.3

--- conf.py
+++ conf.py
@@ -92,7 +92,7 @@ BLOG_DESCRIPTION = "This is a demo site for Nikola."  # (translatable)
 # in the default language will be shown instead.
 
 # What is the default language?
-DEFAULT_LANG = "en"
+DEFAULT_LANG = "zh_cn"
 
 # What other languages do you have?
 # The format is {"translationcode" : "path/to/translation" }
